a2f56c741fee8aad8ead5bd78d07416baba8c204
Fatima20027 <fatima.hailou@icloud.com>
2025-02-03 22:51:06 +0100

    i finished page interface and wrote script for yes button

---
+++ script.js
@@ -0,0 +1,39 @@
+// Function to handle button click events
+const yesBtn = document.getElementById('yes');
+const noBtn = document.getElementById('no');
+const gif = document.getElementById('gif');
+const gifContainer = document.querySelector('.tenor-gif-embed');
+const text = document.getElementById('text');
+const smallText = document.getElementById('smalltext');
+
+yesBtn.addEventListener('click', function() {
+    flashRainbowColors(function() {
+        document.getElementById('question').style.display = 'none';     
+    });
+
+    text.innerText = 'I love you too!💗';
+    smallText.innerText = 'YAAY I kneeeeew it!!';
+    gif.src = 'cat-heart.gif';
+    
+
+
+});
+
+function flashRainbowColors(callback) {
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+    let i = 0;
+    const interval = setInterval(function() {
+        document.body.style.backgroundColor = colors[i];
+        i = (i + 1) % colors.length;
+    }, 200);
+
+    setTimeout(function() {
+        clearInterval(interval);
+        document.body.style.backgroundColor = '';
+        if (callback) {
+            callback();
+        }
+    }, 2000);
+
+    
+}
